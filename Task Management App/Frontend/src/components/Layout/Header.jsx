543a6c4d3db2e27b32c65f8eac66c1234b1e12d6
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-12 15:49:11 +0500

    Working..

--- Task Management App/Frontend/src/components/Layout/Header.jsx
+++ Task Management App/Frontend/src/components/Layout/Header.jsx
@@ -1,46 +1,53 @@
-import React from 'react'
+import React from "react";
 import useAuth from "../../context/AuthContext";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Button from "@/components/ui/button";
 
-
 const Header = () => {
-  const { user, logout }  = useAuth();
+  const { user, logout } = useAuth();
 
   return (
     <>
-    <header className='bg-white shadow-sm dark:bg-gray-800'>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-4">
-          <Link to="/" className='flex itms-center'>
-          <span className="text-xl font-bold text-gray-900 dark:text-white">
-            Task Manager
-          </span>
-          </Link>
+      <header className="bg-white shadow-sm dark:bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center py-4">
+            <Link to="/" className="flex itms-center">
+              <span className="text-xl font-bold text-gray-900 dark:text-white">
+                Task Manager
+              </span>
+            </Link>
 
-          <nav className="flex items-center space-x-4">
-            {user ? (
-              <>
-              <Link to="/dashboard">
-              <Button variant="ghost">
-              Dashobard
-              </Button>
-              </Link>
+            <nav className="flex items-center space-x-4">
+              {user ? (
+                <>
+                  <Link to="/dashboard">
+                    <Button variant="ghost">Dashobard</Button>
+                  </Link>
 
-              <Link to="/tasks">
-              <Button variant="ghost">
-               Tasks
-              </Button>
-              </Link>
-              </>
-            )}
-          </nav>
+                  <Link to="/tasks">
+                    <Button variant="ghost">Tasks</Button>
+                  </Link>
+
+                  <Button onClick={logout} variant="outline">
+                    Logout
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">
+                    <Button variant="ghost">Login</Button>
+                  </Link>
+                  <Link to="/signup">
+                    <Button variant="outline">Sign Up</Button>
+                  </Link>
+                </>
+              )}
+            </nav>
+          </div>
         </div>
-      </div>
-
-    </header>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
