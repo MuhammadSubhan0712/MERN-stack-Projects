4d4c2795b7cc8e00fc3ccf505550d721d5c3a532
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-12 16:10:20 +0500

    Working..

--- Task Management App/Frontend/src/App.jsx
+++ Task Management App/Frontend/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from './components/ProtectedRoute';
+import Dashboard from './pages/Dashboard';
+import Tasks from './pages/Tasks';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { Toaster } from "sonner";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div className='text-center text-red-400 '>App</div>
-  )
-}
+    <>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
 
-export default App
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/tasks" element={<Tasks />} />
+              </Route>
+            </Routes>
+            <Toaster />
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default App;
