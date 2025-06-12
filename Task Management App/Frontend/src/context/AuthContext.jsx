8c0fa28feb9c26bb9e20b1207ac4bc719d098d86
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-12 12:49:53 +0500

    working..

--- Task Management App/Frontend/src/context/AuthContext.jsx
+++ Task Management App/Frontend/src/context/AuthContext.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { createContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import api from '../api';
+import { toast } from "@/components/ui/use-toast";
 
-const AuthContext = () => {
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  
+  // Check auth status initial load:
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const { data } = await api.get('/users/me');
+        setUser(data.data);
+
+      } catch (error) {
+        setUser(null);
+      }
+    }
+    checkAuth();
+  }, []);
+
+  const loginMutation = useMutation({
+    mutationFn: (credentials) => api.post('/auth/login', credentials),
+    onSuccess: (data) => {
+      setUser(data.data.user);
+      localStorage.setItem('token', data.data.token);
+      queryClient.invalidateQueries();
+      navigate('/dashboard');
+      toast({ title: 'Login successful' });
+    },
+    onError: (error) => {
+      toast({
+         title: 'Login failed',
+        description: error.response?.data?.message || 'Something went wrong',
+        variant: 'destructive'
+      });
+    }
+  })
+  
   return (
-    <div>AuthContext</div>
+    <>
+    </>
   )
 }
 
-export default AuthContext
+export default AuthProvider
