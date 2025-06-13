69aa0bcd1309337b033fb8299786e75a16a2d138
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-13 15:05:54 +0500

    Working..

--- Task Management App/Frontend/src/components/UI/Loader.jsx
+++ Task Management App/Frontend/src/components/UI/Loader.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-center h-full">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+      </div>
+    </>
+  );
+};
 
-export default Loader
+export default Loader;
