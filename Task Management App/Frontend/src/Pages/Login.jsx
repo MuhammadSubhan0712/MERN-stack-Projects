00ad14b9efb665f661a960847dfb7ba363b11368
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-13 14:58:40 +0500

    Working.

--- Task Management App/Frontend/src/Pages/Login.jsx
+++ Task Management App/Frontend/src/Pages/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <CardTitle className="text-center">Login to your account</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+            <div className="mt-4 text-center text-sm">
+              Don't have an account?
+              <Link
+                to="/signup"
+                className="font-medium text-primary hover:underline">
+                <Signup></Signup>
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
