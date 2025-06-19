35d905cad6e55b0b309e97ffb8eba989c3d25b27
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-19 11:57:00 +0500

    Testing..

--- Task Management App/Backend/src/models/user.model.js
+++ Task Management App/Backend/src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema(
 
 //Password hashing middleware:
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("passowrd")) return next();
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
   next();
