c8a3412eb6b91acfc0091f75bf7a0b1ebda01b59
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-19 11:17:22 +0500

    Working..

--- Task Management App/Backend/src/routes/auth.route.js
+++ Task Management App/Backend/src/routes/auth.route.js
@@ -4,13 +4,12 @@ import authController from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+
+
 router.use(authController.protect);
-
-
-router
-.route("/")
-.post(authController.signup)
-.post(taskController.createTask);
 
 
 module.exports = router ;
