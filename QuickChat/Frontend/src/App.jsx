1b4d84f14ded5917011c59e3fc8aa5cfc29c13d4
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-20 23:11:59 +0500

    Working.

--- QuickChat/Frontend/src/App.jsx
+++ QuickChat/Frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
- <>
- <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
- </>
-  )
-}
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
