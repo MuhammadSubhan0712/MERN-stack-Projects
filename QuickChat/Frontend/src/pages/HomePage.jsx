0fd074fd6722bbd9353daf63c5584e202a58ff6c
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-21 00:19:32 +0500

    Working..

--- QuickChat/Frontend/src/pages/HomePage.jsx
+++ QuickChat/Frontend/src/pages/HomePage.jsx
@@ -15,9 +15,9 @@ const HomePage = () => {
         <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
          overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
         'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-          <Sidebar />
-          <ChatContainer />
-          <RightSidebar />
+          <Sidebar selectedUser={selectedUser} setSelectedUser={selectedUser} />
+          <ChatContainer selectedUser={selectedUser} setSelectedUser={selectedUser} />
+          <RightSidebar selectedUser={selectedUser} setSelectedUser={selectedUser} />
         </div>
       </div>
     </>
