7393e067f302ad9ee07f59d68d3948f40fe2427e
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-21 00:25:16 +0500

    Working..

--- QuickChat/Frontend/src/components/ChatContainer.jsx
+++ QuickChat/Frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
 
-const ChatContainer = () => {
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
   return (
-    <div>ChatContainer</div>
-  )
-}
+    <>
+      <div>
+        <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+          <img
+            src={assets.profile_martin}
+            alt="profile"
+            className="w-8 rounded-full"
+          />
+          <p>
+            Martin Johnson
+            <span className="w-2 h-2 rounded-full bg-green-500"></span>
+          </p>
+          <img
+          onClick={() => }
+            src={assets.arrow_icon}
+            alt="arrow"
+            className="md:hidden max-w-7"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
