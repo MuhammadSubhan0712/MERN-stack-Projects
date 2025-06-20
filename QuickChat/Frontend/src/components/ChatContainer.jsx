0929a567f33ee3f96cbfc3afc24a1292853c6914
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-21 00:40:12 +0500

    Working..

--- QuickChat/Frontend/src/components/ChatContainer.jsx
+++ QuickChat/Frontend/src/components/ChatContainer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import assets from "../assets/assets";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
-  return (
+  return selectedUser ? (
     <>
-      <div>
+      <div className="h-full overflow-scroll relative backdrop-blur-lg">
         <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
           <img
             src={assets.profile_martin}
@@ -16,14 +16,26 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
             <span className="w-2 h-2 rounded-full bg-green-500"></span>
           </p>
           <img
-          onClick={() => }
+            onClick={() => setSelectedUser(null)}
             src={assets.arrow_icon}
             alt="arrow"
             className="md:hidden max-w-7"
           />
+          <img
+            src={assets.help_icon}
+            alt="help"
+            className="max-md:hidden max-w-5"
+          />
         </div>
       </div>
     </>
+  ) : (
+    <div
+      className="flex flex-col items-center justify-center gap-2 text-gray-500
+    bg-white/10 max-md:hidden">
+      <img src={assets.logo_icon} className="max-w-16" alt="logo" />
+      <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
+    </div>
   );
 };
 
