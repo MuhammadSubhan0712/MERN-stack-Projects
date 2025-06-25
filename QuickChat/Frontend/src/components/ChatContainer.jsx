bc41fc973ecf4c1092b09e4b67c4d69436f78cae
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-25 16:30:10 +0500

    Working..

--- QuickChat/Frontend/src/components/ChatContainer.jsx
+++ QuickChat/Frontend/src/components/ChatContainer.jsx
@@ -1,33 +1,61 @@
 import React from "react";
-import assets from "../assets/assets";
+import assets, { messagesDummyData } from "../assets/assets";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
   return selectedUser ? (
-      <div className="h-full overflow-scroll relative backdrop-blur-lg">
-        <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
-          <img
-            src={assets.profile_martin}
-            alt="profile"
-            className="w-8 rounded-full"
-          />
-          <p>
-            Martin Johnson
-            <span className="w-2 h-2 rounded-full bg-green-500"></span>
-          </p>
-          <img
-            onClick={() => setSelectedUser(null)}
-            src={assets.arrow_icon}
-            alt="arrow"
-            className="md:hidden max-w-7"
-          />
-          <img
-            src={assets.help_icon}
-            alt="help"
-            className="max-md:hidden max-w-5"
-          />
-        </div>
+    <div className="h-full overflow-scroll relative backdrop-blur-lg">
+      {/* Header */}
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+        <img
+          src={assets.profile_martin}
+          alt="profile"
+          className="w-8 rounded-full"
+        />
+        <p>
+          Martin Johnson
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+        <img
+          onClick={() => setSelectedUser(null)}
+          src={assets.arrow_icon}
+          alt="arrow"
+          className="md:hidden max-w-7"
+        />
+        <img
+          src={assets.help_icon}
+          alt="help"
+          className="max-md:hidden max-w-5"
+        />
       </div>
 
+      {/* Chat area */}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${
+              msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"
+            }`}>
+            {msg.image ? (
+              <img
+                src={msg.image}
+                alt="image"
+                className="max-w-[230px] border border-gray-700 rounded-lg oveflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId !== "680f50e4f10f3cd28382ecf9"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}>
+                {msg.text}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
   ) : (
     <div
       className="flex flex-col items-center justify-center gap-2 text-gray-500
