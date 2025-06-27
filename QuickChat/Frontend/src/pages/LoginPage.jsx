7a7a7b974a36d6249662d85ce2fedf0a5dcb7648
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-28 01:02:04 +0500

    Working..

--- QuickChat/Frontend/src/pages/LoginPage.jsx
+++ QuickChat/Frontend/src/pages/LoginPage.jsx
@@ -33,14 +33,52 @@ const LoginPage = () => {
             />
           </h2>
 
-          {currentState === "Sign up" && (
+          {currentState === "Sign up" && !isDataSubmitted && (
             <input
+              onChange={(e) => setFullName(e.target.value)}
+              value={fullName}
               type="text"
               className="p-2 border-gray-500 rounded-md focus:outline-none"
               placeholder="Full Name"
               required
             />
           )}
+
+          {isDataSubmitted && (
+            <>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Email Address"
+                required
+                className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="*********"
+                required
+                className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </>
+          )}
+
+          {currentState === "Sign up" && isDataSubmitted && (
+            <textarea
+              onChange={(e) => setBio(e.target.value)}
+              value={bio}
+              rows={4}
+              className="p-2 border border-gray-500 rounded-md 
+              focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              placeholder="provide a short bio..."></textarea>
+          )}
+
+          <button className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer">
+            {currentState === "Sign up" ? "Create Account" : "Login now"}
+          </button>
         </form>
       </div>
     </>
