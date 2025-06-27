2d6917a308732927079f37e29f985ff9ae24b85e
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-28 00:48:51 +0500

    Working..

--- QuickChat/Frontend/src/pages/LoginPage.jsx
+++ QuickChat/Frontend/src/pages/LoginPage.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import assets from "../assets/assets";
 const LoginPage = () => {
   const [currentState, setCurrentState] = useState("Sign up");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bio, setBio] = useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
   return (
     <>
@@ -27,6 +32,15 @@ const LoginPage = () => {
               className="w-5 cursor-pointer"
             />
           </h2>
+
+          {currentState === "Sign up" && (
+            <input
+              type="text"
+              className="p-2 border-gray-500 rounded-md focus:outline-none"
+              placeholder="Full Name"
+              required
+            />
+          )}
         </form>
       </div>
     </>
