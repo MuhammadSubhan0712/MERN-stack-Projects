f8ce8edf4633b7c1eaf6239aed2573ae327f7f05
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-28 00:43:21 +0500

    Working..

--- QuickChat/Frontend/src/pages/LoginPage.jsx
+++ QuickChat/Frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets";
 const LoginPage = () => {
+  const [currentState, setCurrentState] = useState("Sign up");
+
   return (
     <>
       <div
@@ -17,7 +19,14 @@ const LoginPage = () => {
         <form
           className="border-2 bg-white/8 text-white border-gray-500 p-6 flex 
         flex-col gap-6 rounded-lg shadow-lg">
-          <h2>  </h2>
+          <h2 className="font-medium text-2xl flex justify-between items-center">
+            {currentState}
+            <img
+              src={assets.arrow_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </h2>
         </form>
       </div>
     </>
