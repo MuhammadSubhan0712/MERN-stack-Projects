eba1ca228b076193faa3b8421a6ee866bfdf5f6b
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-28 00:39:22 +0500

    Working..

--- QuickChat/Frontend/src/pages/LoginPage.jsx
+++ QuickChat/Frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import assets from "../assets/assets";
 const LoginPage = () => {
   return (
     <>
-    <div className='min-h-screen bg-cover bg-center flex items-center 
-    justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      <div
+        className="min-h-screen bg-cover bg-center flex items-center 
+    justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+        {/* Left side */}
+        <img
+          src={assets.logo_big}
+          alt="logo"
+          className="w-[min-(30vw, 250px)]"
+        />
 
-      {/* Left Side */}
-      
-      
-    </div>
+        {/* Right side */}
+        <form
+          className="border-2 bg-white/8 text-white border-gray-500 p-6 flex 
+        flex-col gap-6 rounded-lg shadow-lg">
+          <h2>  </h2>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
