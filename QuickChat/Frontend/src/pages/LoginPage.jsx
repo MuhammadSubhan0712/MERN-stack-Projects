ca18eedd27b033ba15a70daffba9580e2d062316
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-06-28 01:37:45 +0500

    Working..

--- QuickChat/Frontend/src/pages/LoginPage.jsx
+++ QuickChat/Frontend/src/pages/LoginPage.jsx
@@ -83,6 +83,24 @@ const LoginPage = () => {
             <input type="checkbox" />
             <p>Agree to the terms of use & privacy policy. </p>
           </div>
+
+          <div className="flex flex-col gap-2">
+            {currentState === "Sign up" ? (
+              <p className="text-sm text-gray-600">
+                Already have an account?{" "}
+                <span className="font-medium text-violet-500 cursor-pointer">
+                  Login here
+                </span>
+              </p>
+            ) : (
+              <p className="text-sm text-gray-600">
+                Create an account{" "}
+                <span className="font-medium text-violet-500 cursor-pointer">
+                  Click here
+                </span>
+              </p>
+            )}
+          </div>
         </form>
       </div>
     </>
