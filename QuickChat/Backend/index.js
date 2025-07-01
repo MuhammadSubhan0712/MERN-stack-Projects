4d7340649481c51cdd2c14a5099d1dd7e394543f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-01 13:31:54 +0500

    Working..

--- QuickChat/Backend/index.js
+++ QuickChat/Backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
 
+dotenv.config();
 // Create express app and http server:
 const app = express();
 const server = http.createServer(app);
@@ -14,4 +15,4 @@ app.use(cors());
 app.use("/api/status", (req, res) => res.send("Server is live"));
 
 const PORT = process.env.PORT;
-server.listen(PORT, () => console.log("Server is running on port:", PORT));
+server.listen(PORT, () => console.log("📡 Server is running on port: 🔗", PORT , "✅"));
