8078bf7a8059db5f20431d00031a40c86abbcb75
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-01 19:50:18 +0500

    Working..

---
+++ QuickChat/Backend/src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+// Function to connect to the mongodb database:
+export const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("🔗 💯 MONGODB connected sucessfully✅")
+    );
+    await mongoose.connect(`${process.env.MONGO_URI}/chatapp`);
+  } catch (error) {
+    console.log("❌ MONGODB connection failed ❌", error);
+  }
+};
