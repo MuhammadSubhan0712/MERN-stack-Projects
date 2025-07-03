d7f053b288e100608b2bd35052b1e1465aff487a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-03 10:11:26 +0500

    Working..

--- QuickChat/Backend/src/routes/message.route.js
+++ QuickChat/Backend/src/routes/message.route.js
@@ -4,6 +4,7 @@ import {
   getMessages,
   getUserForSidebar,
   markMessageAsSeen,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const messageRouter = express.Router();
@@ -13,5 +14,7 @@ messageRouter.get("/users", protectedRoute, getUserForSidebar);
 messageRouter.get("/:id", protectedRoute, getMessages);
 
 messageRouter.put("/mark/:id", protectedRoute, markMessageAsSeen);
+
+messageRouter.post("/send/:id", protectedRoute, sendMessage);
 
 export default messageRouter;
