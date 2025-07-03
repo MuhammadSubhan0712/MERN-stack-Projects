99346f5d5dd50549c2cb3ac615a06ca56ebc25dd
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-03 09:21:23 +0500

    Working.

--- QuickChat/Backend/src/controllers/message.controller.js
+++ QuickChat/Backend/src/controllers/message.controller.js
@@ -37,3 +37,50 @@ export const getUsersForSidebar = async (req, res) => {
     });
   }
 };
+
+// Get all messages for selected user:
+export const getMessages = async (req, res) => {
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
+    await Message.updateMany(
+      { senderId: selectedUserId, receiverId: myId },
+      { seen: true }
+    );
+
+    res.json({
+      success: true,
+      messages,
+    });
+  } catch (error) {
+    console.log("Erorr occured ==> ", error.message);
+    res.json({
+      success: false,
+      message: "Error occured ==>" + error.message,
+    });
+  }
+};
+
+// Api to mark message as seen using message id:
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Message.findByIdAndUpdate(id, { seen: true });
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log("Erorr occured ==> ", error.message);
+    res.json({
+      success: false,
+      message: "Error occured ==>" + error.message,
+    });
+  }
+};
