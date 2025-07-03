52c395e5443422283ed8222d96d2aef5cfb6b43f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-03 10:07:14 +0500

    Working..

--- QuickChat/Backend/index.js
+++ QuickChat/Backend/index.js
@@ -7,7 +7,6 @@ import userRouter from "./src/routes/user.route.js";
 import messageRouter from "./src/routes/message.route.js";
 import { Server } from "socket.io";
 
-
 dotenv.config();
 // Create express app and http server:
 const app = express();
@@ -15,7 +14,7 @@ const server = http.createServer(app);
 
 // Initialize socket.io server:
 export const io = new Server(server, {
-  cors: {origin: "*"}
+  cors: { origin: "*" },
 });
 
 // Store online users:
@@ -25,17 +24,18 @@ export const userSocketMap = {}; // { userId: socketId }
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   console.log("User connected", userId);
-  
-  if (userId)  userSocketMap[userId] = socket.id;
+
+  if (userId) userSocketMap[userId] = socket.id;
 
   // Emit online users to all connected clients:
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log();
-    
-  })
-})
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 // Middleware setup:
 app.use(express.json({ limit: "4mb" }));
@@ -53,4 +53,3 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log("📡 Server is running on port: 🔗", PORT, "✅")
 );
-
