476093bed30383b8b0a824fd6d24c536714fa5f7
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-15 09:10:14 +0500

    Working...

--- QuickChat/Frontend/context/AuthContext.jsx
+++ QuickChat/Frontend/context/AuthContext.jsx
@@ -20,11 +20,34 @@ export const AuthProvider = ({ children }) => {
       const { data } = await axios.get("/api/auth/check");
       if (data.success) {
         setAuthUser(data.user);
+        connectSocket(data.user);
       }
     } catch (error) {
       toast.error(error.message);
     }
   };
+
+  //   Login function to handle user authentication and socket connection:
+  const login = async (state, credentials) => {
+    try {
+      const { data } = await axios.post(`/api/auth/${state}`, credentials);
+      if (data.success) {
+        setAuthUser(data.userData);
+        connectSocket(data.userData);
+        axios.defaults.headers.common["token"] = data.token;
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        toast.success(data.message);
+      }
+      else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+//   
 
   //   Connect socket function to handle socket connection and online users updates:
 
