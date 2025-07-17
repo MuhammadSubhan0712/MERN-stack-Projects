5d4e202a1f58a552f1c7ee5e11cc66c7ab6ea537
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-18 02:08:19 +0500

    Working.

--- QuickChat/Frontend/src/pages/HomePage.jsx
+++ QuickChat/Frontend/src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
+import { ChatContext } from "../../context/ChatContext";
 
 const HomePage = () => {
 
-  const [selectedUser, setSelectedUser] = useState(null);
-  const [showSidebar, setShowSidebar] = useState(true);
+  const { selectedUser } = useContext(ChatContext);
 
   return (
     <>
@@ -16,7 +16,7 @@ const HomePage = () => {
         'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
           <Sidebar />
           <ChatContainer />
-          <RightSidebar selectedUser={selectedUser} setSelectedUser={selectedUser} />
+          <RightSidebar />
         </div>
       </div>
     </> 
