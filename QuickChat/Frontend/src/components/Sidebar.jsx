5a8f5f9c07e9895bdf3152c1a4500de44ada9fc8
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-07-17 17:02:44 +0500

    Working.

--- QuickChat/Frontend/src/components/Sidebar.jsx
+++ QuickChat/Frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import assets, { userDummyData } from "../assets/assets";
+import React, { useContext, useEffect, useState } from "react";
+import assets from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
@@ -15,8 +15,15 @@ const Sidebar = () => {
   } = useContext(ChatContext);
 
   const { logout, onlineUsers } = useContext(AuthContext);
+  const [input, setInput] = useState(false);
 
   const navigate = useNavigate();
+
+  const filteredUsers = input ? users.filter((user) => user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
+
+  useEffect(() => {
+        getUsers();
+  },[onlineUsers])
 
   return (
     <>
@@ -48,6 +55,7 @@ const Sidebar = () => {
           <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
             <img src={assets.search_icon} alt="search" className="w-3" />
             <input
+              onChange={(e) => setInput(e.target.value)}
               type="text"
               className="bg-transparent border-none outline-none
             text-white text-xs placeholder-[#c8c8c8] flex-1"
@@ -57,7 +65,7 @@ const Sidebar = () => {
         </div>
 
         <div className="flex flex-col">
-          {userDummyData.map((user, index) => (
+          {filteredUsers.map((user, index) => (
             <div
               onClick={() => {
                 selectedUser(user);
@@ -73,17 +81,17 @@ const Sidebar = () => {
               />
               <div className="flex flex-col leading-5">
                 <p>{user.fullName}</p>
-                {index < 3 ? (
+                {onlineUsers.includes(user._id) ? (
                   <span className="text-green-400 text-xs">Online</span>
                 ) : (
                   <span className="text-neutral-400 text-xs">Offline</span>
                 )}
               </div>
-              {index > 2 && (
+              {unseenMessages[user._id] > 0 && (
                 <p
                   className="absolute top-4 right-4 text-xs h-5 w-5 
                    flex justify-center items-center rounded-full bg-violet-500/50">
-                  {index}
+                  {unseenMessages[user._id]}
                 </p>
               )}
             </div>
