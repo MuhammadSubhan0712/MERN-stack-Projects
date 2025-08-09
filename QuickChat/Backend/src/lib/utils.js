627c5dcb78fdfaeee58a5b9f8b0a48d647cf9acd
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-08-10 02:39:58 +0500

    Deploying on vercel

--- QuickChat/Backend/src/lib/utils.js
+++ QuickChat/Backend/src/lib/utils.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Function to generate a token for a user:
- const generateToken = (userId) => {
+const generateToken = (userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET);
   return token;
 };
