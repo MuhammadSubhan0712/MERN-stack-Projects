08f553aee30d41d733353258737a5e655d65d996
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-08-10 02:49:34 +0500

    Deploying on vercel

--- QuickChat/Backend/src/middleware/auth.js
+++ QuickChat/Backend/src/middleware/auth.js
@@ -2,7 +2,9 @@
 
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const protectedRoute = async (req, res, next) => {
   try {
     const token = req.headers.token;
